fca541b04808232e9655d9b71af16927bd5bf15b
Nurbolat <nurb01dep@gmail.com>
2023-04-08 13:28:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: 'John',
+//     age: '25',
+//     city: 'Astana'
+// }
+// console.log(obj['name'])
+
+// let arr = ['assasa','asdasd', 5, {}, []]
+// console.log(arr[4])
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result)
+
+console.log(6%2)
+
