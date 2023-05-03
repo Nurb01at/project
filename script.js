a8c4edeb1becd6d657854d4adb1349c025f97853
Nurbolat <nurb01dep@gmail.com>
2023-05-03 13:43:03 +0600

    callback

--- script.js
+++ script.js
@@ -73,3 +73,68 @@ const test = "12.2px"
 console.log(parseInt(test))
 
 console.log(parseFloat(test))
+// =================================================== callback
+
+function first(){
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+function second(){
+    console.log(2)
+}
+
+first();
+second();
+// -----------------------------------------
+
+function learnJS(lang, callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done(){
+    console.log('Я прошел этот урок!')
+}
+// -------------------------
+learnJS('JavaScript', done)
+// ---------------|анонимное функция|----
+learnJS('JavaScript', function(){
+    console.log('Я прошел этот урок!')
+});
+
+// =================================================
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"])
+
+// delete options.name;
+
+// console.log(options)
+
+let counter = 0;
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+            // counter++;
+        }
+    }
+        else {
+            console.log(`Свойство ${key} имеет значение ${options[key]}`)
+            counter++;
+    }
+}
+console.log(counter)
